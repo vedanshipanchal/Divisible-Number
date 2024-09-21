7fe13860830bf8a9131f7c825764596d9620d51d
vedanshipanchal <vedanshipanchal@gmail.com>
2024-09-21 17:42:34 +0100

    Divisible Number

---
+++ main.py
@@ -0,0 +1,10 @@
+print("Enter numerator:")
+num = int(input())
+
+print("Enter donominator:")
+den = int(input())
+
+if num%den==0:
+    print(num,"id divisible by",den)
+else:
+    print("not divisible")
